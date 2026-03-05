6a900ab0074d19c9c386c37d01b5972ecf86f401
Ratul-Raha <ratulraha.04@gmail.com>
2026-03-05 19:38:14 +0600

    hash router problem solving

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -10,7 +9,7 @@ import SingleProduct from './pages/SingleProduct';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import Nav from './components/Nav';
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import Footer from './components/Footer';
 
 const theme = {
@@ -20,26 +19,23 @@ const theme = {
   }
 }
 
-
 const App = () => {
   return (
-    <div>  
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-     <Nav/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/service' element={<Service/>} />
-        <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
-        <Route path='/cart' element={<Cart/>} /> 
-        <Route path='*' element={<NotFound/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-  </ThemeProvider>  
-    </div>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Nav/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/service' element={<Service/>} />
+          <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
+          <Route path='/cart' element={<Cart/>} /> 
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+        <Footer/>
+      </HashRouter>
+    </ThemeProvider>
   );
 };
 
